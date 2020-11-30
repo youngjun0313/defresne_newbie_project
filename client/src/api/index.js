e1f07381ed2b2a9e8fba9901caa3db3f951bff9f
unknown <zerojun0313@kaist.ac.kr>
2020-11-30 20:33:42 +0900

    .env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080/posts';
+const url = 'http://localhost:51313/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => (axios.post(url, newPost));
